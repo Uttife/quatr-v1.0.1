850dea35dd8986e09ff87db88443276de0171e1c
shotayoBoluwatife <shotaybolu@gmail.com>
2023-07-28 11:58:20 +0100

    Second App Model

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() => runApp(const App());
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
   
-  
+  git init
 
   @override
   Widget build(BuildContext context) {
